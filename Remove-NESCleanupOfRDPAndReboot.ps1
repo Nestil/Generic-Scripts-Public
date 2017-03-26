d75529fae85b5fc8f9b3762e66fcb42f96551845
Ron Kjernell <ron@nestil.se>
2017-03-26 15:08:35 +0200

    Script for handling the problem with Client side Rendering
    This script should be placed in the customers RDP server as a scheduled
    event.
    
    It will do the following:
    1. It warns the users on the server that e reboot will happen within 10
    minutes.
    2. logs off all users
    3. stop spooler
    4. remove the values regarding client side rendering from the registry
    5. start spooler
    6. reboot.

---
+++ Remove-NESCleanupOfRDPAndReboot.ps1
@@ -0,0 +1,10 @@
+﻿<#
+  .Synopsis
+    Logs out all users from Remote Desktop and then cleans registry from Client Side Rendering entries, then restarts server.
+  
+  .Notes
+    Author: Ron Kjernell - ron@nestil.se    Dependent on the script: C:\Repo\Generic-Scripts-Public\Disconnect-AndLogOffAllUsersFromRDP.ps1     Which is used to log off all users from the remote desktop before continuing.      .LINK    http://www.nestil.se      https://github.com/Nestil/#>#Let any logged on users know that the server will restart in 10 minutesMSG * "The server will restart for update and cleanup within 10 minutes."#Wait for 10 minutesStart-Sleep -Seconds 600#Log off all users from Remote DesktopC:\Repo\Generic-Scripts-Public\Disconnect-AndLogOffAllUsersFromRDP.ps1Start-Sleep -Seconds 10#Stop the Print spoolerStop-Service SpoolerStart-Sleep -Seconds 10#Remove Reg-values for Client Side RenderingRemove-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Print\Providers\Client Side Rendering Print Provider' -Force -Recurse
+Remove-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Print\Printers\CSR|*' -Force -Recurse
+Remove-Item -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\Print\Providers\Client Side Rendering Print Provider' -Force -Recurse
+Remove-Item -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\Print\Printers\CSR|*' -Force -Recurse
+#Start SpoolerStart-Service Spooler Start-Sleep -Seconds 10#Restart ComputerRestart-Computer -Force
