6012996af63367f7bddd84cb4a99b3207b206f56
Ronkjernell79 <ron@nestil.se>
2017-04-06 10:49:15 +0200

    Small adjustments

--- AD/New-NBHTSServer.ps1
+++ AD/New-NBHTSServer.ps1
@@ -1,16 +1,21 @@
-﻿<#
-  .Synopsis
-    This script sdds all the OU's, adds adminusers to the ou's. 
-    Creates the GPO's and imports the correct GPO's from c:\Repo\Generic-Scripts-Public\AD\GPO\
-    Then links the GPO's to Correct OU
-  
-  .Notes
-    Author: Ron Kjernell - ron@nestil.se        Things to add in the future: Create the shares and set correct rights on these.         .LINK    http://www.nestil.se      https://github.com/Nestil/#>
-
+﻿Import-Module RemoteDesktop
 
 [xml]$AD = Get-Content -Path C:\Repo\Generic-Scripts-Public\AD\XML\AD.xml
-Import-Module ActiveDirectory
 $Netbiosname = $AD.AD.Netbios
 $FirstOU = $AD.AD.FirstOU
+$TSName = Read-Host "Write the Netbios name of your TS server" 
+$Localmachine = hostname
 
-Add-WindowsFeature -Name "Search-Service" 
+
+#Deploy Connection broker, Webaccessserver and sessionhost to your TS server 
+New-RDSessionDeployment -ConnectionBroker "$TSName.$Netbiosname.local" -WebAccessServer "$TSName.$Netbiosname.local" -SessionHost "$TSName.$Netbiosname.local"
+¨#Wait 5 minutes due to server restart
+Start-Sleep 300
+
+#Add Licensingserver on this machine, Should be the DC in most cases
+Add-RDServer -Server "$Localmachine.$Netbiosname.local" -Role RDS-LICENSING -ConnectionBroker "$TSName.$Netbiosname.local"
+
+Set-RDLicenseConfiguration -LicenseServer "$Localmachine.$Netbiosname.local" -Mode PerUser -ConnectionBroker "$TSName.$Netbiosname.local"
+
+#Create the Collection
+New-RDSessionCollection -CollectionName SessionCollection -SessionHost "$TSName.$Netbiosname.local" -CollectionDescription "Remote Desktop Collection" -ConnectionBroker "$TSName.$Netbiosname.local"
