09df24cc3b407c9b15654025c9e4beb6bf238e14
tomme93 <tommy@nestil.se>
2017-10-31 15:12:01 +0100

    New Script
    Change permissions on room calendar.

---
+++ O365/Set-NESAddOrganizerToSubjectRoomCalendar.ps1
@@ -0,0 +1,16 @@
+﻿Set-ExecutionPolicy RemoteSigned
+
+$LiveCred = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange-ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+
+cls
+
+Get-Mailbox -Filter '(RecipientTypeDetails -eq "RoomMailBox")' | Select Name
+
+$RoomMailbox=Read-Host "Please enter the room to edit"
+
+Set-MailboxFolderPermission -AccessRights LimitedDetails -Identity ${RoomMailbox}:\calendar -User default
+
+Set-CalendarProcessing -Identity $RoomMailbox -AddOrganizerToSubject $true -DeleteComments $false -DeleteSubject $false
+
