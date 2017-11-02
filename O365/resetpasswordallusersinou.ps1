ad951079b2ab64167b41aa4eddc581140126845d
MikaelWijk <mikael@nestil.se>
2017-11-02 09:39:10 +0100

    Test
    Test

--- O365/resetpasswordallusersinou.ps1
+++ O365/resetpasswordallusersinou.ps1
@@ -1,1 +1,2 @@
-﻿DSQUERY user "OU=myOU,OU=myUsers,DC=myDomain,DC=loc" -limit 0 | DSMOD user -pwd <insert new password here>
+﻿#Nestil Script
+DSQUERY user "OU=myOU,OU=myUsers,DC=myDomain,DC=loc" -limit 0 | DSMOD user -pwd <insert new password here>
